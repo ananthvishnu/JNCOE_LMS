976b84c08d10d8619d369c13db3c8e4fe62b6599
Ananthakumar Vishnurathan <124557188+ananthvishnu@users.noreply.github.com>
2024-08-16 22:14:01 +0530

    lms completed

---
+++ client/app/utils/CoursePlayer.tsx
@@ -0,0 +1,50 @@
+import React, { FC, useEffect, useState } from "react";
+import axios from "axios";
+
+type Props = {
+  videoUrl: string;
+  title: string;
+};
+
+const CoursePlayer: FC<Props> = ({ videoUrl }) => {
+  const [videoData, setVideoData] = useState({
+    otp: "",
+    playbackInfo: "",
+  });
+
+  useEffect(() => {
+    axios
+      .post(`${process.env.NEXT_PUBLIC_SERVER_URI}getVdoCipherOTP`, {
+        videoId: videoUrl,
+      })
+      .then((res) => {
+        setVideoData(res.data);
+      }
+    );
+
+  }, [videoUrl]);
+
+  return (
+    <div
+      style={{ position: "relative", paddingTop: "56.25%", overflow: "hidden" }}
+    >
+      {videoData.otp && videoData.playbackInfo !== "" && (
+        <iframe
+          src={`https://player.vdocipher.com/v2/?otp=${videoData?.otp}&playbackInfo=${videoData.playbackInfo}&player=I7g6x59kJ6fUX8vJ`}
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            border: 0,
+          }}
+          allowFullScreen={true}
+          allow="encrypted-media"
+        ></iframe>
+      )}
+    </div>
+  );
+};
+
+export default CoursePlayer;
